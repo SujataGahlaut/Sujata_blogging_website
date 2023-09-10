613024d2c55120952d08e51f2bb1aef909981947
SujataGahlaut <120093947+SujataGahlaut@users.noreply.github.com>
2023-09-10 18:51:52 +0530

    Add files via upload
    added client files

---
+++ Header.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "./UserContext";
+
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  // The useEffect hook you provided will be called when the component in which
+  // it resides mounts for the first time.In other words, it runs the code inside the effect function after the initial render of the component.The[] dependency array you passed as the second argument specifies the conditions under which the effect should run.
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
